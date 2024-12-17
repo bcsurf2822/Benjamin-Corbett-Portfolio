4e333ce6d65f140389ce899adbd847eaadb448f9
ocneb <111916951+bcsurf2822@users.noreply.github.com>
2024-12-17 16:24:21 -0500

    rem bg from projects

--- app/projects/page.js
+++ app/projects/page.js
@@ -2,9 +2,7 @@ import ProjectCarousel from "./ProjectCarousel";
 
 export default function Projects() {
   return (
-<section id="projects"
-      className="mt-2 flex bg-neutral-50 h-[100vh]  items-center"
-    >
+    <section id="projects" className="mt-2 flex h-[100vh]  items-center">
       <ProjectCarousel />
     </section>
   );

53541205a51d19af0a25d08d1d8d3d7f7c7f8dfe
Benjamin Corbett <benjamincorbett@Benjamins-iMac.local>
2025-02-28 13:03:10 -0500

    initial commit: working on scroll to sections

--- app/about/page.js
+++ app/about/page.js
@@ -3,7 +3,7 @@ import AboutCards from "../components/AboutCards";
 
 export default function About() {
   return (
-    <section id="about" className="relative z-0">
+    <section id="about" className="relative z-0 bg-neutral-200 scroll-mt-[80px] ">
       <AboutCards />
     </section>
   );
--- app/projects/page.js
+++ app/projects/page.js
@@ -2,7 +2,7 @@ import ProjectCarousel from "./ProjectCarousel";
 
 export default function Projects() {
   return (
-    <section className="flex h-[100vh]  items-center">
+    <section className="flex h-[100vh]  items-center bg-neutral-50 ">
       <ProjectCarousel />
     </section>
   );

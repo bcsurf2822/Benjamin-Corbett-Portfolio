98debbc8bff2fd0b71fea8ef7385995cc5996925
Benjamin Corbett <benjamincorbett@Benjamins-iMac.local>
2025-03-03 09:14:27 -0500

    moved card data into own file

--- app/components/AboutCards.js
+++ app/components/AboutCards.js
@@ -1,18 +1,6 @@
 "use client";
 import { motion } from "framer-motion";
-
-const life = [
-  "I am a full-stack developer who enjoys practicing my current skill set by building responsive and scalable applications.",
-  "Outside of tech, I enjoy going to the beach, surfing, skateboarding, spending time with my wife and dog, cooking, hiking, traveling, and doing DIY projects around the house.",
-  "Before getting into software, I was a commercial fisherman for 12 years, fishing out of many ports in the country including Alaska, Massachusetts and California.",
-  "My time there has taught me countless skills such as leadership, responsibility, and the benefits of hard work.",
-  "I've had the privelage to work and laugh with some of the best people and have expereienced so much during our trips at sea.",
-];
-
-const tech = [
-  "While recovering from an injury sustained on the boat I took my passion for technology to the next level by using online resources to teach myself HTML, JavaScript, and CSS. In 2023 I joined Parsity, an online coding bootcamp, where I built a strong foundation in React, Redux, MongoDB, Express, and Node.",
-  "Since finishing my course work, I have been practicing with those technologies on a daily basis while working to master my skills in NextJS.  My current interests are TypeScript, AI integration, and Amazon Web Services(AWS).  My goal is to work alongside a great team at a company where I can continue to grow as a person and developer.",
-];
+import { life, tech } from "../data/aboutData";
 
 const AboutCards = () => {
   return (

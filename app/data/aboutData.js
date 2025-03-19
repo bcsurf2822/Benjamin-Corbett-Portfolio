48a3a94fd70255fa25cacb7b7f84524cf74202a3
Benjamin Corbett <benjamincorbett@Benjamins-iMac.local>
2025-03-19 07:52:08 -0400

    fixed about section

--- app/data/aboutData.js
+++ app/data/aboutData.js
@@ -1,17 +1,15 @@
 export const tech = [
-  "I am a Full-Stack Software Developer with a strong foundation in JavaScript development, specializing in building responsive web applications using a combination of React, Redux, Next.js, MongoDB, Express.js, and Node.js.",
+  "I am a Full-Stack Software Developer with a strong foundation in JavaScript development. I specialize in building responsive web applications using React, Redux, Next.js, MongoDB, Express.js, and Node.js.",
 
-  "My journey into software began while recovering from an injury sustained on a fishing vessel.  During that time I leveraged online resources to teach myself HTML, JavaScript, and CSS.",
+  "My journey into software began while recovering from an injury sustained on a fishing vessel.  During that time I used online resources to teach myself HTML, JavaScript, and CSS.",
 
-  "In 2023, I enrolled in Parsity, an online coding bootcamp, where I continued to develop my skills in web development, learning front-end frameworks and libraries, workplace best practices, and back-end solutions.",
+  "I enjoyed the material that I was learning, so in 2023, I enrolled in Parsity, where I continued to develop my skills in web development, front-end frameworks and libraries, workplace best practices, and back-end technologies.",
 
-  "I enjoy practicing on a daily basis and am currently expanding my skill set in TypeScript, Ai integration, and Amazon Web Services.",
+  "I continue to practice with these technologies on a regular basis and am currently expanding my skill set in AI integration, and Amazon Web Services.",
 ];
 
-export const life = [  
-  "Before getting into software, I was a commercial fisherman for 12 years, fishing off the coasts of Alaska, Massachusetts and California.  This experience provided me with life skills such as leadership, resilience, teamwork, and the ability to stay calm in high stress environments.",
+export const life = [
+  "Before  software, I worked as commercial fisherman for 12 years, fishing off the coasts of Alaska, New Jersey Massachusetts and California.  This experience provided me with key skills such as leadership, resilience, teamwork, and the ability to stay calm in high stress environments.",
 
-  "My life is centered around quality time at home with my family, wife, and dog where I enjoy home improvement projects and cooking.",
-
-  "Outside of tech, I enjoy an active lifestyle where I'm passionate about surfing, skateboarding, traveling, hiking, reading, and yoga.",
+  "Outside of tech, I enjoy time with my wife and dog, DIY projects around the house, cooking going to the beach, surfing, camping, surfing, skateboarding, traveling, hiking, reading, and yoga.",
 ];
